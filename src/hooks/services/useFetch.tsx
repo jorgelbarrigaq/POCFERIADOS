829d6ef175fc2b954687a3c17112f4e16f45068a
Jorge Leonidas Barriga Quilodran <jbarriga@Jorges-MacBook-Air.local>
2023-08-23 00:33:54 -0400

    Release 4

--- src/hooks/services/useFetch.tsx
+++ src/hooks/services/useFetch.tsx
@@ -1,20 +1,9 @@
-import {useEffect,useState} from 'react'
-import axios from "axios";
-import {IFeriados} from './IFeriados.tsx'
+
 
 export function useFetch(url:string)
 {
-    const [data, setData] = useState<IFeriados[]>([]);
-    const [loading, setLoading] = useState(null);
-    const [error, setError] = useState(null);
+   
 
-     useEffect(()=>{
-
-        setLoading(null);
-       
-       
-    },[])
-
-    return { data, loading, error}
+    return url;
 
 }
